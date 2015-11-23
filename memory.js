aa5f2dc5874969e8eb9bc32b9af6c3a787584c1b
adriane26 <adrianepurdy@yahoo.com>
2015-11-23 14:44:22 -0800

    Added scoring feature. Need to stylize game, add reset button, add input for player names

--- memory.js
+++ memory.js
@@ -13,9 +13,19 @@ var memory_array =[
 	"donutella",
 	"skeletrinalg"
 	];
+
 var memory_values = [];
+
 var memory_tile_ids = [];
+
 var tiles_flipped = 0;
+
+var turnCount = 0;
+
+var player1Score = 1;
+var player2Score = 1;
+var playerID = null;
+
 Array.prototype.memory_tile_shuffle = function(){
     var i = this.length, j, temp;
     while(--i > 0){
@@ -25,6 +35,8 @@ Array.prototype.memory_tile_shuffle = function(){
         this[i] = temp;
     }
 };
+
+
 function newBoard(){
 	tiles_flipped = 0;
 	var output = '';
@@ -33,26 +45,42 @@ function newBoard(){
 		output += '<div class="tile" id="tile_'+i+'" onclick="memoryFlipTile(this,\''+memory_array[i]+'\')"></div>';
 	}
 	document.getElementById('gameboard').innerHTML = output;
-	console.log(output);
+	//console.log(output);
 };
+
+
+
 function memoryFlipTile(tile,val){
-	if(tile.innerHTML == "" && memory_values.length < 2){
-		//tile.style.background = '#FFF';
+	if(tile.innerHTML === "" && memory_values.length < 2){
+		tile.style.background = '#FFF';
 		tile.innerHTML = '<img class="card-img img-reponsive" src="images/'+ val + '.png"/>';
-		if(memory_values.length == 0){
+		//console.log(tile.innerHTML);
+		if(memory_values.length === 0){
 			memory_values.push(val);
 			memory_tile_ids.push(tile.id);
-			console.log(this);
-		} else if(memory_values.length == 1){
+			//console.log(this);
+		} else if(memory_values.length === 1){
 			memory_values.push(val);
 			memory_tile_ids.push(tile.id);
-			if(memory_values[0] == memory_values[1]){
+			if(memory_values[0] === memory_values[1]){
 				tiles_flipped += 2;
 				// Clear both arrays
 				memory_values = [];
             	memory_tile_ids = [];
+
+            	/////////// ADDED
+            	turnCount ++;
+            	console.log("turnCount"+turnCount);
+            	setTimeout(currentPlayer,500);
+            	//////
+            	if (playerID ===1){
+            		$('#player1Score').html(player1Score++);
+            	} else {
+            		$('#player2Score').html(player2Score++);
+            	}
+
 				// Check to see if the whole board is cleared
-				if(tiles_flipped == memory_array.length){
+				if(tiles_flipped === memory_array.length){
 					alert("Let's play again!");
 					document.getElementById('gameboard').innerHTML = "";
 					newBoard();
@@ -71,7 +99,43 @@ function memoryFlipTile(tile,val){
             	    memory_tile_ids = [];
 				}
 				setTimeout(flip2Back, 500);
+
+				//// added
+					turnCount ++;
+					setTimeout(currentPlayer,700);
+					console.log("turnCount"+turnCount);
 			}
 		}
 	}
 };
+
+/*
+
+var player1Turn = function(){
+	memoryFlipTile();
+	if turnCount
+}
+
+
+*/
+
+var currentPlayer = function(){
+	if (turnCount %2 === 0) {
+		playerID = 1;
+		alert("player1, your turn");
+		console.log(playerID);
+	} else {
+		playerID = 2;
+		alert("player 2, your turn");
+		console.log(playerID);
+	}
+};
+
+
+////// IN SCORING SECTION: IF CURRENT PLAYER IS 1, update 
+//// how to check which is current player? just update current player's score. 
+
+
+
+
+
