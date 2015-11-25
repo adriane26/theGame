00bbe361022c21787e4d52e6ad7aeef64b4184f6
adriane26 <adrianepurdy@yahoo.com>
2015-11-24 23:13:26 -0800

    cleaned up and commented code, final (for now) working copy

--- memory.js
+++ memory.js
@@ -1,4 +1,4 @@
-
+/////// Declaring variables
 var memory_array =[
 	"donutella",
 	"porcinolg",
@@ -13,19 +13,16 @@ var memory_array =[
 	"bastardinojrlg",
 	"skeletrinalg"
 	];
-
 var memory_values = [];
-
 var memory_tile_ids = [];
-
 var tiles_flipped = 0;
-
 var turnCount = 0;
-
 var player1Score = 1;
 var player2Score = 1;
 var playerID = null;
 
+
+//////// Shuffles memory tile array
 Array.prototype.memory_tile_shuffle = function(){
     var i = this.length, j, temp;
     while(--i > 0){
@@ -36,7 +33,7 @@ Array.prototype.memory_tile_shuffle = function(){
     }
 };
 
-
+////// Sets up a new board
 function newBoard(){
 	tiles_flipped = 0;
 	var output = '';
@@ -45,72 +42,71 @@ function newBoard(){
 		output += '<div class="tile" id="tile_'+i+'" onclick="memoryFlipTile(this,\''+memory_array[i]+'\')"></div>';
 	}
 	document.getElementById('gameboard').innerHTML = output;
-	//console.log(output);
 };
 
-
+////// Game play. A whole lotta game play. Memory values array holds the values of the 2 cards that are flipped. if they match, the appropriate player's score is increased and it's the next player's turn. If they don't match, the next player goes.
 
 function memoryFlipTile(tile,val){
 	if(tile.innerHTML === "" && memory_values.length < 2){
 		tile.style.background = 'none';
-		tile.innerHTML = '<img src="images/'+ val + '.png"/>';
-		//console.log(tile.innerHTML);
+		tile.innerHTML = '<img src="images/'+ val +'.png"/>';
+		
 		if(memory_values.length === 0){
 			memory_values.push(val);
 			memory_tile_ids.push(tile.id);
-			//console.log(this);
+		
 		} else if(memory_values.length === 1){
 			memory_values.push(val);
 			memory_tile_ids.push(tile.id);
+
+			////// if it's a match... 
 			if(memory_values[0] === memory_values[1]){
 				tiles_flipped += 2;
 				// Clear both arrays
 				memory_values = [];
             	memory_tile_ids = [];
 
-            	/////////// ADDED
+            	/////////// TurnCount is increased and the next player is up
             	turnCount ++;
-            	console.log("turnCount"+turnCount);
             	setTimeout(currentPlayer,500);
-            	//////
+            	////// increase the appropriate player's score
             	if (playerID ===1){
-            		$('#player1Score').html(player1Score++);
-            		
-
-
-
-
+            		$('#player1Score').html(player1Score++);	
             	} else {
             		$('#player2Score').html(player2Score++);
             	}
 
-				// Check to see if the whole board is cleared
+				///// Check to see if the whole board is cleared/matched. If so, declare the winner!
 				if(tiles_flipped === memory_array.length){
 					if (player1Score > player2Score) {
-							swal({   title: "Player 1: #winning",   text: "Let's play again!",   imageUrl: "images/StrawberryMilkLgcopy.png" });
+						swal({   title: "Player 1: #winning",   text: "Let's play again!",   imageUrl: "images/StrawberryMilkLgcopy.png" });
 
-						//alert("Nice job, Player 1! Let's play again!")
-
+						///// was having trouble with sweetAlert, leaving this commented normal alert in for now.
+						//alert("Player 1: #winning. Let's play again!");   
 
 					} else if (player2Score > player1Score) {
 
-								swal({   title: "Heck yeah, Player 2!",   text: "Let's play again!",   imageUrl: "images/tigercopy.png" });
+							swal({   title: "Heck yeah, Player 2!",   text: "Let's play again!",   imageUrl: "images/tigercopy.png" });
 
-						//alert("Way to go, Player 2! Let's play again!")
+							///// was having trouble with sweetAlert, leaving this commented normal alert in for now.
+						//alert("Heck yeah, Player 2! Let's play again!");
 
 
 					} else {
-					swal({   title: "It's a tie!",   text: "Let's play again!",   imageUrl: "images/sabochan.png" });
-						//alert("It's a tie! Let's play again!")
+						swal({   title: "Well look at that. You're both winners.",   text: "Let's play again!",   imageUrl: "images/sabochan.png" });
+
+						///// was having trouble with sweetAlert, leaving this commented normal alert in for now, just in case.
+						//alert("Well look at that. You're both winners! Let's play again!");
 
 					};
-
-
+					/////// clear the board, reset the turnCount, set newBoard.
 					document.getElementById('gameboard').innerHTML = "";
 					turnCount = 0;
 					newBoard();
 				}
 			} else {
+
+				///// if it's not a match... 
 				function flip2Back(){
 				    // Flip the 2 tiles back over
 				    var tile_1 = document.getElementById(memory_tile_ids[0]);
@@ -125,47 +121,36 @@ function memoryFlipTile(tile,val){
 				}
 				setTimeout(flip2Back, 500);
 
-				//// added
+				//// increase the turn count, switch players.
 					turnCount ++;
 					setTimeout(currentPlayer,500);
-					//currentPlayer();
-					console.log("turnCount"+turnCount);
+				
 			}
 		}
 	}
 };
 
-/*
-
-var player1Turn = function(){
-	memoryFlipTile();
-	if turnCount
-}
-
-
-*/
+////////  whose turn is it? 
 var currentPlayer = function(){
 	if (turnCount %2 === 0) {
 		playerID = 1;
-		// alert("player1, your turn");  highlight playername
-		//$('#player1Div').style.textShadow="3px 2px #3FF245";
+		//////// add text shadow highlight to player 1, remove from player 2.
 		$('#player1Div').css({"textShadow": "3px 2px #3FF245"});
 		$('#player2Div').css({"textShadow": ""});
-		console.log(playerID);
+		
 	} else {
 		playerID = 2;
-		//alert("player 2, your turn");
+		////// if it's 2nd player's turn, highlight player 2 name and remove highlight from player 1.
 		$('#player2Div').css({"textShadow": "3px 2px #3FF245"});
 		$('#player1Div').css({"textShadow": ""});
 		console.log(playerID);
 	}
 };
 
-///////  RESET BUTTON
 
+///////  RESET BUTTON (same as reloading the page)
 
 $('#resetButton').on('click', function(){  
-	//newBoard();
 	turnCount =0;
 	$('#player1Score').html('&nbsp;');
 	$('#player2Score').html('&nbsp');
@@ -180,54 +165,28 @@ $('#resetButton').on('click', function(){
 });
 
 
-////// IN SCORING SECTION: IF current player is 1, highlight animation OR JUST ADD SHADOW TEXT!!
-//
 
-
-
-
-////// mouseover tkdk image
-
-/*
-
-function showLogo(x) {
-    x.style.height = "64px";
-    x.style.width = "64px";
-};
-function normText() {
-
-}
-
-////// event Listeners
-
-player1Input: .submit(e.preventDefault)
-
-var player1Name = function(name) {
-
-}
-
-*/
-
-
-////////// when you click playButton, all of this happens. ////////
+////////// when you click playButton, all of this happens.
 
 $('#playButton').on('click', function(){
 
-	///// capture name values, add to player1Div and player2Div. RESET BUTTON NEEDS TO CLEAR THIS INFO from html
+	///// capture name values, add to player1Div and player2Div.
 	var player1Text = $('#player1Name').val();
 	var player2Text = $('#player2Name').val();
+
 	///// add value to player1div/player2div. if no input, default to "Player 1" and "Player 2"
 		if (!player1Text) {
 			$('#player1P').html('Player 1');
 		} else {
 				$('#player1P').html(player1Text);
 		};
+
 		if (!player2Text) {
 			$('#player2P').html('Player 2');
 		} else {
 				$('#player2P').html(player2Text);
 		};
-////// clear input values
+	////// clear input values
 	$('#player1Name').val('');
 	$('#player2Name').val('');
 
@@ -236,13 +195,12 @@ $('#playButton').on('click', function(){
 	$('#headerTitle').hide();
 
 	///// highlight player 1 div to start
-		$('#player1Div').css({"textShadow": "3px 2px #3FF245"});
+	$('#player1Div').css({"textShadow": "3px 2px #3FF245"});
+
 		////// finally, playButton will call newBoard();
-		newBoard();
+	newBoard();
 
-
-
-});  //// create another nbsp in <p></p> instead of player1? 
+}); 
 
 
 
